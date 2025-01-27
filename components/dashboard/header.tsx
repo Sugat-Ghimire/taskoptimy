e49220e8bf5d9e8ef75a1346fd543a314c8bb528
Sugat Ghimire <sugat1237@gmail.com>
2025-01-27 21:40:54 +0545

    added dropdown menu for kanban board selection in header

--- components/dashboard/header.tsx
+++ components/dashboard/header.tsx
@@ -3,7 +3,7 @@
 import { useState } from "react";
 import { motion } from "framer-motion";
 import { Input } from "@/components/ui/input";
-import { Search, UserPlus, Bell } from "lucide-react";
+import { Search, UserPlus, Bell, ChevronDown } from "lucide-react";
 import { ModeToggle } from "./mode-toggle";
 import { Button } from "@/components/ui/button";
 import { useSession } from "next-auth/react";
@@ -11,6 +11,8 @@ import { useRouter } from "next/navigation";
 import Image from "next/image";
 
 export function Header({ activeSection }: { activeSection: string }) {
+  console.log(activeSection);
+
   const [isSearchOpen, setIsSearchOpen] = useState(false);
   const router = useRouter();
   const { data: session, status } = useSession();
@@ -32,6 +34,30 @@ export function Header({ activeSection }: { activeSection: string }) {
           >
             Welcome to TaskOptimy!
           </motion.h2>
+        ) : activeSection === "kanban Board" ? (
+          <div className="relative group">
+            <button className="flex items-center space-x-2 text-2xl font-semibold capitalize">
+              <span>{activeSection}</span>
+              <ChevronDown className="w-5 h-5" />
+            </button>
+
+            <div className="z-10 absolute left-0 mt-2 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 invisible group-hover:opacity-100 group-hover:visible transition-all duration-200">
+              <div className="py-1">
+                <a
+                  href="#"
+                  className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                >
+                  Project Board 1
+                </a>
+                <a
+                  href="#"
+                  className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                >
+                  Create New Board
+                </a>
+              </div>
+            </div>
+          </div>
         ) : (
           <h2 className="text-2xl font-semibold capitalize">{activeSection}</h2>
         )}
